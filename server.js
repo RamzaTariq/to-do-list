48c62cde0218e2649f11faa6aefad905c9cdfcf3
Ramza <ramza_tariq@outlook.com>
2026-02-11 15:33:15 +0000

    second server commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const app - express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -11,3 +11,18 @@ let task = [];
 app.get("/tasks",(req, res) =>{
     res.json(tasks);
 });
+
+//POST new task 
+app.post("/tasks",(req, res) => {
+    const tasks = req.body;
+    res.status(201).json(task);
+});
+
+//DELETE task
+app.delete("/tasks/:id", (req, res) => {
+    const id = req.params.id;
+    tasks = tasks.filter(t => t.id != id);
+    res.json({message: "Task deleted"});
+});
+
+app.listen(3000,() => console.log("API running"));
